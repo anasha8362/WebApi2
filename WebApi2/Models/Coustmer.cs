16c9a356a0af9194ed578d9dde1813aefd8372b9
anasha8362 <63153203+anasha8362@users.noreply.github.com>
2021-04-14 01:24:19 +0500

    modifyedAdd project files.

---
+++ WebApi2/Models/Coustmer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi2.Models
+{
+    public class Coustmer
+    {
+        public int CoustmerId { get; set; }
+
+        public string CoustmerName { get; set; }
+
+        public string DateOfJioning { get; set; }
+     
+        public string PhotoFileName { get; set; }
+
+        public string Department { get; set; }
+    }
+}
